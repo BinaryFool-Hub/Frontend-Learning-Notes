504b3ca8129e83be6e82722341274fe72dce868e
BinaryFool <daishibing2006@gmail.com>
2025-04-27 17:33:03 +0800

    更改了登录按钮事件名称

--- 收藏/login/login.js
+++ 收藏/login/login.js
@@ -2,7 +2,7 @@ layui.use(function () {
     var form = layui.form;
     var layer = layui.layer;
     // 提交事件
-    form.on('submit(demo-login)', function (data) {
+    form.on('submit(login)', function (data) {
         var field = data.field; // 获取表单字段值
         // 显示填写结果，仅作演示用
         console.log(JSON.stringify(field));
